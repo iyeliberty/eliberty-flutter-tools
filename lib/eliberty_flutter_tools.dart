8544257a491a370f20f4aa485b998c57b9d080bd
Ilyes Yhy <ilyes.yahyaoui@eliberty.fr>
2025-05-16 04:28:26 +0200

    fix: exposed authcubit instead

--- lib/eliberty_flutter_tools.dart
+++ lib/eliberty_flutter_tools.dart
@@ -1,5 +1,5 @@
-export 'features/auth/domain/usecases/sign_in_with_email.dart';
-export 'features/auth/domain/usecases/sign_out.dart';
+export 'features/auth/presentation/cubit/auth_cubit.dart';
+export 'features/auth/presentation/cubit/auth_state.dart';
 export 'features/auth/domain/entities/user.dart';
 export 'core/errors/failures.dart';
 export 'core/injection/injection.dart' show initElibertyFlutterTools;
